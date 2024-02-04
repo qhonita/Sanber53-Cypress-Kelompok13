312a56d9c98a930f70dab85fe1a6ec141d5a8201
sitialawiah <alawiahsiti13@gmail.com>
2024-02-04 20:51:03 +0700

    add baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://magento.softwaretestingboard.com/",
+    env: {
+      email: "kelompok13@gmail.com"
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },

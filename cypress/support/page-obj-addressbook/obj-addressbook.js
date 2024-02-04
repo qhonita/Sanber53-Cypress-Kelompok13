e58a07801c2adf14b444ec6e52cd705598ea404e
mbagasrw <mbagasrw@users.noreply.github.com>
2024-02-04 22:51:50 +0700

    update_1 addressbook

---
+++ cypress/support/page-obj-addressbook/obj-addressbook.js
@@ -0,0 +1,16 @@
+class addressBook{
+    actionedit = '.action.edit'
+    firstname = '#firstname'
+    lastname = '#lastname'
+    company = '#company'
+    telephone = '#telephone'
+    street_1 = '#street_1'
+    street_2 = '#street_2'
+    street_3 = '#street_3'
+    country = '#country'
+    city = '#city'
+    region_id = '#region_id'
+    zip = '#zip'
+    actionsaveprimary = '.action.save.primary'
+}
+export default new addressBook

9ab71d415d1717490375718c0ee443609d007630
qhonita <qhonitaanif@gmail.com>
2024-02-04 16:32:42 +0700

    update page object register

---
+++ cypress/support/pageObject-register/registerPage.js
@@ -0,0 +1,10 @@
+class registerPage{
+    firstname = '[id="firstname"]'
+    lastname = '[id="lastname"]'
+    email = '[name="email"]'
+    password = '#password'
+    passconfirm = '#password-confirmation'
+    submit = '.submit.submit'
+}
+export default new registerPage
+

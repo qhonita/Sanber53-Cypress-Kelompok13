05370e8b8ef70deba5d4f692120434807a4c8bca
mbagasrw <mbagasrw@users.noreply.github.com>
2024-02-06 21:19:55 +0700

    update testcase addressbook

--- cypress/e2e/3-web/magento/myaccount-addressbook.cy.js
+++ cypress/e2e/3-web/magento/myaccount-addressbook.cy.js
@@ -11,10 +11,14 @@ describe('Address Book', () => {
     cy.get(objAddressbook.email).type('tes.mbagasrw@yopmail.com')
     cy.get(objAddressbook.pass).type('Welcome@123')
     cy.get(objAddressbook.btn_login).click()
-    cy.wait(5000)
+    cy.wait(10000)
     cy.xpath("/html/body/div[2]/header/div[1]/div/ul/li[2]/span/button", {timeout: 5000, multiple: true}).click()
     cy.xpath("//div[@aria-hidden='false']//a[normalize-space()='My Account']", {timeout: 10000}).click()
     cy.xpath("//div[@class='column main']//div[@class='block block-dashboard-addresses']//div[@class='block-content']//div[@class='box box-billing-address']//div[@class='box-actions']//a[@class='action edit']/span",{ multiple: true }).click()
+    //cy.visit('https://magento.softwaretestingboard.com/customer/account/')
+    //cy.get(':nth-child(10) > a').click()
+    //cy.url().should('contain','/customer/address/')
+    //objAddressbook.clickLogin()
   })
 
   it('Passed to Edit Address Book', () => {
@@ -40,28 +44,25 @@ describe('Address Book', () => {
     cy.get(objAddressbook.zip).clear()
     cy.get(objAddressbook.zip).type('12345')
     cy.get(objAddressbook.btn_SaveAddressBook).click()
-    //cy.screenshot('Screenshot-Passed to Edit Address Book')
+    cy.wait(1000)
   })
 
   it('Edit Address Book - First Name Empty', () => {
     cy.get(objAddressbook.firstname).clear()
     cy.get(objAddressbook.btn_SaveAddressBook).click()
     cy.get('#firstname-error').should('be.visible').and('contain.text', 'This is a required field.')
-    //cy.screenshot('Screenshot-Passed to Edit Address Book')
   })
 
   it('Edit Address Book - Last Name Empty', () => {
     cy.get(objAddressbook.lastname).clear()
     cy.get(objAddressbook.btn_SaveAddressBook).click()
     cy.get('#lastname-error').should('be.visible').and('contain.text', 'This is a required field.')
-    //cy.screenshot('Screenshot-Passed to Edit Address Book')
   })
 
   it('Edit Address Book - Telephone Empty', () => {
     cy.get(objAddressbook.telephone).clear()
     cy.get(objAddressbook.btn_SaveAddressBook).click()
     cy.get('#telephone-error').should('be.visible').and('contain.text', 'This is a required field.')
-    //cy.screenshot('Screenshot-Passed to Edit Address Book')
   })
 
   it('Edit Address Book - Street Empty', () => {
@@ -70,21 +71,18 @@ describe('Address Book', () => {
     cy.get(objAddressbook.street_3).clear()
     cy.get(objAddressbook.btn_SaveAddressBook).click()
     cy.get('#street_1-error').should('be.visible').and('contain.text', 'This is a required field.')
-    //cy.screenshot('Screenshot-Passed to Edit Address Book')
   })
 
   it('Edit Address Book - City Empty', () => {
     cy.get(objAddressbook.city).clear()
     cy.get(objAddressbook.btn_SaveAddressBook).click()
     cy.get('#city-error').should('be.visible').and('contain.text', 'This is a required field.')
-    //cy.screenshot('Screenshot-Passed to Edit Address Book')
   })
 
   it('Edit Address Book - ZIP Empty', () => {
     cy.get(objAddressbook.zip).clear()
     cy.get(objAddressbook.btn_SaveAddressBook).click()
     cy.get('#zip-error').should('be.visible').and('contain.text', 'This is a required field.')
-    //cy.screenshot('Screenshot-Passed to Edit Address Book')
     //objAddressbook.verifyError()
   })
 
@@ -104,13 +102,13 @@ describe('Address Book', () => {
     cy.get('#street_1-error').should('be.visible').and('contain.text', 'This is a required field.')
     cy.get('#city-error').should('be.visible').and('contain.text', 'This is a required field.')
     cy.get('#zip-error').should('be.visible').and('contain.text', 'This is a required field.')
-    //cy.screenshot('Screenshot-Failed to Edit Address Book - Mandatory Field Empty')
+    //objAddressbook.verifyError()
   })
 
   it('Edit Address Book - Non Mandatory Field Empty', () => {
     cy.get(objAddressbook.company).clear()
     cy.get(objAddressbook.region).clear()
     cy.get(objAddressbook.btn_SaveAddressBook).click()
-    //cy.screenshot('Screenshot-Failed to Edit Address Book - Non Mandatory Field Empty')
+    cy.wait(2000)
   })
 })
